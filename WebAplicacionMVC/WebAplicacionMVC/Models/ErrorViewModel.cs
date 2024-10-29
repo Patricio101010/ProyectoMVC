24642780c6942b90b8d6423fbe8e6b9507fe9aee
Patricio101010 <patriciommorales@gmail.com>
2024-10-29 17:58:12 -0300

    primeros pasos mvc

---
+++ WebAplicacionMVC/WebAplicacionMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAplicacionMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
